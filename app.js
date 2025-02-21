e2cda8fcc70f95fb034132834a1b347eb79b8c35
priyanshuvatsyan <priyanshuvatsyan@gmail.com>
2025-02-21 17:12:04 +0530

    changed port

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ app.get('/',(req,res)=>{
 const PORT = process.env.PORT || 3000;
 
 
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-});
+app.listen(3000, '0.0.0.0', () => {
+    console.log('Server running on port 3000');
+  });
+  
